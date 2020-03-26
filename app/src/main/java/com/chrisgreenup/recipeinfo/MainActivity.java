f31fb17ba1d22696a15f96bdaee98a538a5fbb3b
cgreenup <charlirejunk@gmail.com>
2020-03-25 21:15:59 -0500

    Rudimentary Shell

--- app/src/main/java/com/chrisgreenup/recipeinfo/MainActivity.java
+++ app/src/main/java/com/chrisgreenup/recipeinfo/MainActivity.java
@@ -2,13 +2,31 @@ package com.chrisgreenup.recipeinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.start_button).setOnClickListener(this);
+        findViewById(R.id.app_info_button).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        if(view.getId() == R.id.start_button) {
+            intent = new Intent(getBaseContext(), RecipeInfoActivity.class);
+        }else{
+            intent = new Intent(getBaseContext(), AboutAppActivity.class);
+        }
+        startActivity(intent);
     }
 }
