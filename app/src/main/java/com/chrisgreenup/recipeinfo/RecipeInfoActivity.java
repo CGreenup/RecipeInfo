ef305a1ada5f604576ad2e7ba84316429a3337df
cgreenup <charlirejunk@gmail.com>
2020-03-26 21:17:16 -0500

    Added a result screen

--- app/src/main/java/com/chrisgreenup/recipeinfo/RecipeInfoActivity.java
+++ app/src/main/java/com/chrisgreenup/recipeinfo/RecipeInfoActivity.java
@@ -1,14 +1,37 @@
 package com.chrisgreenup.recipeinfo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RecipeInfoActivity extends AppCompatActivity {
+public class RecipeInfoActivity extends AppCompatActivity
+implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
+        setupResults();
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.submit_btn){
+            setupResults();
+        }
+        else {
+            setupActivityRecipe();
+        }
+    }
+
+    void setupActivityRecipe(){
+        setContentView(R.layout.activity_recipe);
+        findViewById(R.id.submit_btn).setOnClickListener(this);
+    }
+
+    void setupResults(){
+        setContentView(R.layout.results);
+        findViewById(R.id.result_return_btn).setOnClickListener(this);
+    }
+
 }
