4e9f8290f4b75b1aaedc9236700acdf11174666b
cgreenup <charlirejunk@gmail.com>
2020-03-28 02:43:17 -0500

    Added function where the editText content is put into a file

--- app/src/main/java/com/chrisgreenup/recipeinfo/RecipeInfoActivity.java
+++ app/src/main/java/com/chrisgreenup/recipeinfo/RecipeInfoActivity.java
@@ -1,9 +1,18 @@
 package com.chrisgreenup.recipeinfo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class RecipeInfoActivity extends AppCompatActivity
 implements View.OnClickListener {
@@ -17,7 +26,8 @@ implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.submit_btn){
-            setupResults();
+            //setupResults();
+            handle();
         }
         else {
             setupActivityRecipe();
@@ -32,6 +42,37 @@ implements View.OnClickListener {
     void setupResults(){
         setContentView(R.layout.results);
         findViewById(R.id.result_return_btn).setOnClickListener(this);
+    }
+
+    public void handle(){
+        EditText et = findViewById(R.id.recipe_et);
+        writeData(et);
+
+    }
+
+    private void writeData(EditText editText){
+        try {
+            FileOutputStream fos = openFileOutput("userInput.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            pw.println(editText.getText().toString());
+
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void readData(){
+        try {
+            FileInputStream fis = openFileInput("userInput.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
